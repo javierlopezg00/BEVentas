d25363a0f4487ccdf62b0ebf41dd6a10a124ca5d
Javier Lopez <52970558+javierlopezg00@users.noreply.github.com>
2022-08-25 19:46:01 -0600

    Update InventarioControlador.java

--- src/main/java/net/codejava/Controlador/InventarioControlador.java
+++ src/main/java/net/codejava/Controlador/InventarioControlador.java
@@ -203,6 +203,17 @@ public class InventarioControlador {
 		Optional<Inventario> n = repositorioInventario.findById(nIdInventario);
 		Inventario _n = n.get();
 		
+		int a = 1;
+		
+		if(true){
+			a=1;
+		}
+		if(true){
+			a=2;
+		}
+		if(true){
+			a=3;
+		}
 		
 		if(nOperacion.equals("Agregar")) {
 			
