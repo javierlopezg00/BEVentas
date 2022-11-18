743e69496825e290af9e2c821d67c4d9179e81a1
Javier Lopez <52970558+javierlopezg00@users.noreply.github.com>
2022-11-18 17:47:42 -0600

    Update UsuariosControlador.java

--- src/main/java/net/codejava/Controlador/UsuariosControlador.java
+++ src/main/java/net/codejava/Controlador/UsuariosControlador.java
@@ -99,7 +99,7 @@ public class UsuariosControlador {
 		HashMap<String,String> response = new HashMap<>();
 		
 		
-		response.put("respuesta", "10/28/2000MANANA");
+		response.put("respuesta", "19/11/2021");
 
 		
 		
